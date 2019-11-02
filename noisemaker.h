85c8380faeab2e5d4714aa0e96a060f97e0ce25d
jpear1 <99jackpearson@gmail.com>
2019-11-01 23:08:52 -0700

    outlined Signal and Oscillator

--- noisemaker.h
+++ noisemaker.h
@@ -1,4 +1,32 @@
+// high level overview: Oscillators create sound
 
-class Sound {
-    
-}
+#include <cstdint>
+
+typedef int16_t sample;
+
+class Signal {
+    public:
+        sample samples[10000];
+        int currentSampleIndex;
+        sample currentSample;
+};
+
+class Noisemaker {
+    public:
+        virtual sample getSampleAtTime(int t) = 0;
+};
+
+class Oscillator: Noisemaker {
+    public:
+        sample getSampleAtTime(int t);
+
+        void setInputAmplitude(Signal s);
+        void setInputAmplitude(int i);
+        void setSampleIncrement(Signal s);
+        void setSampleIncrement(int i);
+        void setWaveform(int a[], int size);
+};
+
+class Adder {};
+
+class Multiplier {};
